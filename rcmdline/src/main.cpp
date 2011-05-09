e5a765df08063f31cd5cbec6c9a51d63a954714c
raiden <raiden@gmail.com>
2011-05-09 09:06:17 +0800

    default dict path fixed

--- rcmdline/src/main.cpp
+++ rcmdline/src/main.cpp
@@ -7,14 +7,16 @@
 #include "stardict.h"
 
 
+#define DEFAULT_DICT_PATH "../dictsupport/dict/stardict-langdao-ec-gb-2.4.2/"
+
 int main(int argc, char** argv) {
 	RCore *core = new RCore();
-	if (!core->loadDict("dictsupport/dict/stardict-langdao-ec-gb-2.4.2/")) {
+	if (!core->loadDict(DEFAULT_DICT_PATH)) {
 		printf("error  when loading dict\n");
 		exit(0);
 	}
 	for (int i = 1; i < argc; ++i) {
-		printf("checking word %s\n",argv[i]);
+		printf("checking word \'%s\'\n",argv[i]);
 		printf("%s\n",core->lookUpWord(argv[i]).c_str());
 	}
 	//argvsAnalyse(argc,argv);
